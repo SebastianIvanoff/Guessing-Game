5a16ec82e83254367262c94257692aa83db5bc42
waivan <31401897+waivan@users.noreply.github.com>
2022-09-29 11:22:53 +0200

    Added Function checkGuess() and some html

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,3 +1,16 @@
-let random = Math.floor(Math.random() * 100);
+let random = Math.floor(Math.random() * 100 + 1);
+console.log("Random Number", random);
 
-console.log(random);
+function checkGuess() {
+  let myGuess = guess.value;
+  if (myGuess === random) {
+    feedback.textContent = "Du gissade rätt";
+  } else if (myGuess > random) {
+    feedback.textContent =
+      "Din gissning var " + myGuess + ". Det är för högt. Försök igen!";
+  } else if (my < random) {
+    feedback.textContent =
+      "Din gissning var " + myGuess + ". Det äet är för lågt. Försök igen!";
+  }
+}
+submitGuess.addEventListener("click", checkGuess);
